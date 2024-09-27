fefcf275b74e7bdc62badd42a0c2f2d81f726708
Johann Pouponnot <johann.pouponnot@tyliainvest.com>
2024-09-28 01:42:13 +0200

    chore: rename modules -> module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 /* eslint-disable react-hooks/exhaustive-deps */
-import ChooseRestaurant from "@/modules/restaurant/view/page/ChooseRestaurant"
-import CreateRestaurant from "@/modules/restaurant/view/page/CreateRestaurant"
-import RestaurantsList from "@/modules/restaurant/view/page/RestaurantsList"
-import useRestaurantStore from "@/modules/restaurant/store/restaurantStore"
+import ChooseRestaurant from "@/module/restaurant/view/page/ChooseRestaurant"
+import CreateRestaurant from "@/module/restaurant/view/page/CreateRestaurant"
+import RestaurantsList from "@/module/restaurant/view/page/RestaurantsList"
+import useRestaurantStore from "@/module/restaurant/store/restaurantStore"
 import { ReactElement, useEffect } from "react"
-import useRestaurant from "@/modules/restaurant/src/application/useRestaurant"
+import useRestaurant from "@/module/restaurant/src/application/useRestaurant"
 
 export default function Home() {
   const store = useRestaurantStore()
